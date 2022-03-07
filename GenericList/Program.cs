66ce6fcec50f52798231804f111136f7923d1caa
Bora Kaya <bora.587@hotmail.com>
2022-03-08 00:18:13 +0300

    Add project files.

---
+++ GenericList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> renkler = new List<string>();
+
+            renkler.Add("Mavi");
+            renkler.Add("Kırmızı");
+
+            foreach (var item in renkler)
+            {
+                Console.WriteLine(renkler);
+
+            }
+        }
+    }
+}
